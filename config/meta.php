47828afe18729e08fd65e39653f9efddb46de548
Aditya <adityasaxena602@gmail.com>
2021-03-10 12:00:54 +0530

    remove brands for format restructure

--- config/meta.php
+++ config/meta.php
@@ -1,7 +1,8 @@
 <?php
 
 return [
-    'to_format_brands' => ["Roar + Rabbit", "Amigo Modern"],
+    'to_format_brands' => [],
+    
     'dims_form_feature_brands' => [],
     'sets_enabled_brands' => ['pier1', 'nw'],
     'sets_enabled_tables' => [
