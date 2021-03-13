26523de75582bfd0515c6fd6de547735d2cfbc90
krishnamzmdr <krishna.mzmdr@gmail.com>
2021-03-13 17:43:10 +0530

    Update Product.php

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -895,7 +895,8 @@ class Product extends Model
 
         $min = $price->min('min_price');
 		if($sale_products_only){
-			$max = $price->where('min_price', '!=', 'min_was_price')->max('max_price');
+			$price = $price->where('min_price', '!=', 'min_was_price');
+			$max = $price->max('max_price');
 		}else{
 			$max = $price->max('max_price');
 		}
