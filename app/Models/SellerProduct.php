afc16fe92c44b6136543da0c9f9bdaf8a70e4a68
krishnamzmdr <krishna.mzmdr@gmail.com>
2021-04-21 18:20:51 +0530

    Update SellerProduct.php

--- app/Models/SellerProduct.php
+++ app/Models/SellerProduct.php
@@ -349,6 +349,7 @@ class SellerProduct extends Model
 					$max_price = 0;
 					$min_was_price = 1000000;
 					$max_was_price = 0;
+					return $data['variations'][0];
 					for($i=0;$i<count($data['variations']);$i++){
 						$arr2 = json_decode($data['variations'][$i]);
 						$variation_images = '';
