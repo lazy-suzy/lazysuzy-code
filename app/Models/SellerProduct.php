11892183db23168fba3fb6f6bd0d1d0972a94607
krishnamzmdr <krishna.mzmdr@gmail.com>
2021-04-23 02:12:26 +0530

    Update SellerProduct.php

--- app/Models/SellerProduct.php
+++ app/Models/SellerProduct.php
@@ -1133,14 +1133,16 @@ class SellerProduct extends Model
 					 
 					 $varimgs = '';
 					 $optionimg =[];
-					$var_images_decode = json_decode($row1->image_path);  
-					$row1->image_path = [];
+					 
+					
 					
 					try{
-					for($i=0;$i<count($var_images_decode);$i++){
-						$optionimg[$i] = "https://www.lazysuzy.com/".$var_images_decode[$i];
-					}
-					$row1->image_path = $optionimg;
+						$var_images_decode = json_decode($row1->image_path); 
+						for($i=0;$i<count($var_images_decode);$i++){
+							$optionimg[$i] = "https://www.lazysuzy.com/".$var_images_decode[$i];
+						}
+						 $row1->image_path = [];
+						 $row1->image_path = $optionimg;
 					}
 					catch{}
 					
