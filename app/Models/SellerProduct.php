7925916274a83873b60bd20c3358157582552eb6
krishnamzmdr <krishna.mzmdr@gmail.com>
2021-04-25 08:04:08 +0530

    Update SellerProduct.php

--- app/Models/SellerProduct.php
+++ app/Models/SellerProduct.php
@@ -742,7 +742,7 @@ class SellerProduct extends Model
 							for($i=0;$i<count($data['product_images']);$i++){
 								
 								    $imagedata = SellerProduct::is_base64_encoded($data['product_images'][$i]); 
-									$aqqq[$i] =  $imagedata ;
+									$arr[$i]['imagedata'] =  $imagedata ;
 									 if($imagedata==1){
 
 										$image_parts = explode(";base64,", $data['product_images'][$i]);
@@ -761,7 +761,7 @@ class SellerProduct extends Model
 	
 							}
 					 
-					 return $aqqq.'================'.$arr;
+					 return $arr;
 					}
 					
 					for($i=0;$i<count($data['product_images_names']);$i++){
