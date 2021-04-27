f62861803edbf1a5c38ef229dea54cc66d354177
krishnamzmdr <krishna.mzmdr@gmail.com>
2021-04-27 14:06:17 +0530

    Update Cart.php

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -269,7 +269,7 @@ class Cart extends Model
                     //->where($table . '.' . $parent_sku_field, $row->product_sku) // where parent SKU is given in variations table
 					->where ($table . '.has_parent_sku',1)
                     ->groupBy(Cart::$cart_table . '.product_sku');
-
+return $vrows;
                 $vrows = $vrows->get()->toArray();
  
                 // one parent SKU can have many variations SKUs 
