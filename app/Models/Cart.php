f5fde9e89917c6898ff798e0dbeab996e349b0c4
krishnamzmdr <krishna.mzmdr@gmail.com>
2021-04-27 20:05:40 +0530

    Update Cart.php

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -423,7 +423,7 @@ class Cart extends Model
         $total_cost_rate_shipping = [];
 
         // [brand] => [total_cost for barnd]
-        $total_cart_fixed_shipping = [];//return $cart_rows;
+        $total_cart_fixed_shipping = [];return $cart_rows;
         foreach ($cart_rows as $row => &$product) {
 
             $p_val = $wp_val = $discount = null;
