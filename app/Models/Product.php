e56e56ab74e8eb736efc6d8374b88ddfa9e4dad9
krishnamzmdr <krishna.mzmdr@gmail.com>
2021-05-05 16:58:28 +0530

    Update Product.php

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2134,7 +2134,7 @@ class Product extends Model
                         "features" => $features,
                         "has_parent_sku" => isset($prod->has_parent_sku) ? (bool) $prod->has_parent_sku : false,
                         //"image" => Product::$base_siteurl . $prod->image_path,
-                        "image" => sizeof($imgarr) > 0 ? $imgarr: Product::$base_siteurl,
+                        "image" => sizeof($imgarr) > 0 ? $imgarr[0] : Product::$base_siteurl,
                         "link" =>  "/product/" . $product->product_sku,
                         "swatch_image" => strlen($prod->swatch_image_path) != 0 ? Product::$base_siteurl . $prod->swatch_image_path : null,
                         "price" => $prod->price,
