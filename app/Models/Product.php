1d008e9226d679aa6c1bcb294beef833a3c2d821
krishnamzmdr <krishna.mzmdr@gmail.com>
2021-05-06 15:10:57 +0530

    Update Product.php

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -1596,8 +1596,8 @@ class Product extends Model
 
         $main_image = ($is_details_minimal) ?  $product->image_xbg : $product->main_product_images;
         $product_sub_details_arr = [];
-        return $product->product_sub_details;
-        if(isset($product->product_sub_details)){
+         
+        if(isset($product->product_sub_details) && $product->product_sub_details!=''){
             $subdetarr = json_decode($product->product_sub_details);
             foreach($subdetarr as $psdetails){
                 if(isset($psdetails->image)){
