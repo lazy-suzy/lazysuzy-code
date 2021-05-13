8ecff9c1d3cb442c25d11d58e802592d301de1e6
krishnamzmdr <krishna.mzmdr@gmail.com>
2021-05-13 13:19:51 +0530

    Update DimensionsFilter.php

--- app/Models/DimensionsFilter.php
+++ app/Models/DimensionsFilter.php
@@ -124,8 +124,8 @@ class DimensionsFilter extends Model
 
         $dim_range_list = [];
         foreach($dim_filters as $dimension_type => $obj) {
-            $ranges = self::make_range($obj['min'], $obj['max']);
-            usort($ranges, function ($a, $b) {
+            $range = self::make_range($obj['min'], $obj['max']);
+            usort($range, function ($a, $b) {
                 return $a["min"] > $b["min"];
             });
 
@@ -133,7 +133,7 @@ class DimensionsFilter extends Model
                 $to = $all_filters[strtolower($obj['label']) . '_to']; // $to = array of values
                 $from = $all_filters[strtolower($obj['label']) . '_from']; // from = array of values
             
-                foreach($ranges as &$range) {
+               // foreach($ranges as &$range) {
                     foreach($to as $index => $val) {
                         
                         if (isset($range['checked']) && $range['checked'] == true)
@@ -146,14 +146,14 @@ class DimensionsFilter extends Model
                             $range['checked'] = false;
                     }
                     
-                } 
+                //} 
             }
 
             $dim_range_list[$dimension_type] = [
                 'name' => $obj['label'],
                 'key' => $obj['value'],
                 'enabled' => true,
-                'values' => $ranges
+                'values' => $range
             ];
         }
 
