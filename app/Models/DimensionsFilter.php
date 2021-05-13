b856efcd9848d5c3970fe255d2c3cc3db38ed0d1
krishnamzmdr <krishna.mzmdr@gmail.com>
2021-05-13 14:29:05 +0530

    Update DimensionsFilter.php

--- app/Models/DimensionsFilter.php
+++ app/Models/DimensionsFilter.php
@@ -153,7 +153,7 @@ class DimensionsFilter extends Model
                 'name' => $obj['label'],
                 'key' => $obj['value'],
                 'enabled' => true,
-                'values' => $ranges
+                'values' => json_decode($ranges)
             ];
         }
 
