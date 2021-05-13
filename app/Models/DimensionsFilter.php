fc01dcb412471590db73711c5dcccc4ce72fbe1b
krishnamzmdr <krishna.mzmdr@gmail.com>
2021-05-13 13:04:00 +0530

    Update DimensionsFilter.php

--- app/Models/DimensionsFilter.php
+++ app/Models/DimensionsFilter.php
@@ -124,8 +124,8 @@ class DimensionsFilter extends Model
 
         $dim_range_list = [];
         foreach($dim_filters as $dimension_type => $obj) {
-            $range = self::make_range($obj['min'], $obj['max']);
-            usort($range, function ($a, $b) {
+            $ranges = self::make_range($obj['min'], $obj['max']);
+            usort($ranges, function ($a, $b) {
                 return $a["min"] > $b["min"];
             });
 
@@ -133,36 +133,27 @@ class DimensionsFilter extends Model
                 $to = $all_filters[strtolower($obj['label']) . '_to']; // $to = array of values
                 $from = $all_filters[strtolower($obj['label']) . '_from']; // from = array of values
             
-                //foreach($ranges as &$range) {
+                foreach($ranges as &$range) {
                     foreach($to as $index => $val) {
                         
                         if (isset($range['checked']) && $range['checked'] == true)
-                         continue;
-                        
+                            continue;
 
                         if ((float)$range['min'] == (float) $from[$index] 
-                            && (float)$range['max'] == (float) $to[$index]){
-                                $range['checked'] = true;
-                        }
-                            
+                            && (float)$range['max'] == (float) $to[$index])
+                            $range['checked'] = true;
                         else
                             $range['checked'] = false;
-
-                           
                     }
-                     $checked = $range['checked'];
-                    $min = $range['min'];
-                    $max = $range['max'];
-                    $to = $range['to'];
-                    $from = $range['from'];
-               // } 
+                    
+                } 
             }
- 
+
             $dim_range_list[$dimension_type] = [
                 'name' => $obj['label'],
                 'key' => $obj['value'],
                 'enabled' => true,
-                'min' => $min
+                'values' => $ranges[0]
             ];
         }
 
