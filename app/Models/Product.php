8594e7a4fc44bb4c48d8aa15317d7b0735d890fb
krishnamzmdr <krishna.mzmdr@gmail.com>
2021-05-13 17:54:27 +0530

    Update Product.php

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -472,7 +472,8 @@ class Product extends Model
                     foreach ($get_similar_LS_ID as $Slsid) {
                         $mid = $Slsid->LS_ID;
                         if (isset($categories[$mid])) {
-                            $categories[$mid]['enabled'] = true;
+
+                           // $categories[$mid]['enabled'] = true;
                             if (in_array($categories[$mid]['value'], $in_filter_categories)) {
                                 $categories[$mid]['checked'] = true;
                             }
