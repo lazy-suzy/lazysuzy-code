72fd29fb9cd1a9c84db314b025e1cee3113b831a
krishnamzmdr <krishna.mzmdr@gmail.com>
2021-05-14 12:51:13 +0530

    Changes in user defined min max value

--- app/Models/DimensionsFilter.php
+++ app/Models/DimensionsFilter.php
@@ -132,8 +132,8 @@ class DimensionsFilter extends Model
             $to = $obj['max'];
 
             if(isset($all_filters[strtolower($obj['label']) . '_to'])) {
-                $to = $all_filters[strtolower($obj['label']) . '_to']; // $to = array of values
-                $from = $all_filters[strtolower($obj['label']) . '_from']; // from = array of values
+                $to = (float) $all_filters[strtolower($obj['label']) . '_to']['min']; // $to = array of values
+                $from = (float) $all_filters[strtolower($obj['label']) . '_from']['max']; // from = array of values
             
              /*  foreach($ranges as &$range) {
                     foreach($to as $index => $val) {
