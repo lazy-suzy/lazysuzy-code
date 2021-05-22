9bba94014eff35cd960e2669615e1030c59a3bed
krishnamzmdr <krishna.mzmdr@gmail.com>
2021-05-22 09:41:24 +0530

    Update Product.php

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -1163,8 +1163,8 @@ class Product extends Model
             return [
                 "from" => round($p_from),
                 "to" => round($p_to),
-                "max" => isset($max) ? $max : 0,
-                "min" => isset($min) ? round((float)($min),2) : 0,
+                "max" => isset($max) ? round($max,2) : 0,
+                "min" => isset($min) ? round($min,2) : 0,
             ];
         }
     }
