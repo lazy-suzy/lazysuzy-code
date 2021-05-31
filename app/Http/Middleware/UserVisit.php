f62d7135bd51955565f0d1b4e170894662f129a2
krishnamzmdr <krishna.mzmdr@gmail.com>
2021-05-31 21:34:24 +0530

    Add Product Model and unquote the reverted code

--- app/Http/Middleware/UserVisit.php
+++ app/Http/Middleware/UserVisit.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use App\Models\UserVisits;
+use App\Models\Product;
 use Closure;
 use Auth;
 
@@ -19,10 +20,10 @@ class UserVisit
     {
         $sku = $request->route('sku');
         $user_id = Auth::user()->id;
-       /* $new_sku = Product::get_new_sku($sku);
+        $new_sku = Product::get_new_sku($sku);
         if($new_sku!=false)
             $sku = $new_sku;
-        */
+
         UserVisits::save_user_visit_sku($user_id, $sku);
         return $next($request);
     }
