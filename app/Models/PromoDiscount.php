60f2bb610357aaaed5aa254f16609244f759c987
krishnamzmdr <krishna.mzmdr@gmail.com>
2021-06-17 08:53:59 +0530

    Update PromoDiscount.php

--- app/Models/PromoDiscount.php
+++ app/Models/PromoDiscount.php
@@ -286,7 +286,7 @@ class PromoDiscount extends Model
             if($shipcodeprcnt!=''){
                 $shiparrcount = count($ship_arr)-1;
             }
-             return 'ss='.$shipcodefixed;
+             
             if($shiparrcount<=2){ 
                 if(count($shipcode_arr['wg'])>1){
                     $rate = round($get_shipamount[0]->rate_multi,2);
@@ -306,7 +306,7 @@ class PromoDiscount extends Model
                     }
                    // $total = count($shipcode_arr['wg'])+count($shipcode_arr['sv']);
  
-                  
+                  return $shipcode_arr;
                     if(count($shipcode_arr['wg'])>1 || count($shipcode_arr['sv'])>1){ //|| $total>1
                         $rate = round($get_shipamount[0]->rate_multi,2);
                     }
