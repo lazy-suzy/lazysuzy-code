875a364b9ca048596e41cbe2d60b3cd03d6a2ce7
krishnamzmdr <krishna.mzmdr@gmail.com>
2021-06-17 11:09:55 +0530

    Update PromoDiscount.php

--- app/Models/PromoDiscount.php
+++ app/Models/PromoDiscount.php
@@ -328,9 +328,10 @@ class PromoDiscount extends Model
                 }
                 
                 $temp = $cart['order']['shipment_total']-$rate;
-                 $cart['order']['shipment_total'] = $cart['order']['shipment_total']-$temp ;
-              //  $cart['order']['shipment_total'] = $temp+$getsvcost ; 
-              return $getsvcost.'==='.$temp.'==='.$rate.'==='.$cart['order']['shipment_total'];
+                 $cart['order']['shipment_total'] = $cart['order']['shipment_total']-$temp+$getsvcost ;
+               $cart['order']['shipment_total'] = $temp+$getsvcost ; 
+               return $shipcode_arr;
+               //return $getsvcost.'==='.$temp.'==='.$rate.'==='.$cart['order']['shipment_total'];
 
                 if(($temp<=0 && $shipcode_arr['othercount']>0) || (count($shipcode_arr['wg'])==0 && count($shipcode_arr['sv'])==0)){
                     $cart['order']['shipment_total'] = $temp+$getsvcost ; 
