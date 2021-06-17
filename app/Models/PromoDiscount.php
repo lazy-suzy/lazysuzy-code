b83d1ef1bfadd455b72bf5a625d57632a1e84fce
krishnamzmdr <krishna.mzmdr@gmail.com>
2021-06-17 09:09:13 +0530

    Update PromoDiscount.php

--- app/Models/PromoDiscount.php
+++ app/Models/PromoDiscount.php
@@ -337,7 +337,7 @@ class PromoDiscount extends Model
                 
             } 
         }
- 
+ return $totalcost.'=========';
         if($totalcost>0){ // This calculation is for promo exist brand and particular ship code
           
             $get_shipamount = (new self)->get_ship_rate($promo_details['type_ship']);
