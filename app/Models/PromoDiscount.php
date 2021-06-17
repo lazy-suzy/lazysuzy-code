cda286ac2910076b9015127a8b2d00878366cb82
krishnamzmdr <krishna.mzmdr@gmail.com>
2021-06-17 09:18:59 +0530

    Update PromoDiscount.php

--- app/Models/PromoDiscount.php
+++ app/Models/PromoDiscount.php
@@ -371,9 +371,12 @@ class PromoDiscount extends Model
                 
                 $temp = $cart['order']['shipment_total']-$rate;
             // return $shipcode_arr;
-                $cart['order']['shipment_total'] = $cart['order']['shipment_total']-$temp+$getsvcost ; 
+                 
                 if((count($shipcode_arr['wg'])==0 || count($shipcode_arr['sv'])==0)){
                     $cart['order']['shipment_total'] = $temp+$getsvcost ; 
+                }
+                else{
+                    $cart['order']['shipment_total'] = $cart['order']['shipment_total']-$temp+$getsvcost ;
                 }
                 
             }
