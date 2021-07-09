5bbbb4b78d44f285cff7e121338707e5eb85c522
krishnamzmdr <krishna.mzmdr@gmail.com>
2021-07-09 13:47:20 +0530

    Update SellerMapping.php

--- app/Models/SellerMapping.php
+++ app/Models/SellerMapping.php
@@ -119,6 +119,9 @@ class SellerMapping
             // If edit retrieve master_product from the table, else create a new one.
             if ($this->edit) {
                 $master_product = Product::where('product_sku', $seller_product->product_sku)->first();
+                if($master_product==null){
+                    $master_product = new Product();
+                }
             } else {
                 $master_product = new Product();
             }
